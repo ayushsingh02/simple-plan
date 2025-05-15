b69811a317337c5dfe9fdb8710e78bdfb7f12086
Ayush Singh <code.ayushsingh@gmail.com>
2025-05-15 17:51:07 +0530

    Update footer.jsx

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
         <div className='container text-center'>
         <span class="inline-block h-1 w-10 rounded bg-red-500 mt-8 mb-6"></span>
             <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">
-              SIMPLE PLAN ASSIGNMENT
+              SIMPLE MART
             </h2>
             <p class="text-gray-500 mb-5">&copy; 2025</p>
             
